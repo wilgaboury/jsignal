16cee3f91624ce8e9bd4574f667cdeaeb561634c
Wil Gaboury <wgaboury@gmail.com>
2023-10-03 18:23:22 -0400

    Fix effect synchronization issues

--- src/main/java/com/github/wilgaboury/jsignal/Effect.java
+++ src/main/java/com/github/wilgaboury/jsignal/Effect.java
@@ -3,8 +3,6 @@ package com.github.wilgaboury.jsignal;
 import java.lang.ref.Cleaner;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,29 +17,30 @@ public class Effect implements Runnable {
     private final Runnable effect;
     private final Cleanup cleanup;
     private final Cleaner.Cleanable cleanable;
-    private final AtomicBoolean disposed;
     private final Long threadId;
+
+    private boolean disposed;
 
     Effect(Runnable effect, boolean isSync) {
         this.id = nextId.getAndIncrement();
         this.effect = effect;
-        this.cleanup = new Cleanup(isSync ? new ArrayDeque<>() : new ConcurrentLinkedQueue<>());
+        this.cleanup = new Cleanup();
         this.cleanable = cleaner.register(this, cleanup);
-        this.disposed = new AtomicBoolean(false);
         this.threadId = isSync ? Thread.currentThread().getId() : null;
+        this.disposed = false;
     }
 
     public int getId() {
         return id;
     }
 
-    public void dispose() {
-        if (disposed.compareAndExchange(false, true))
-            cleanable.clean();
+    public synchronized void dispose() {
+        disposed = true;
+        cleanable.clean();
     }
 
-    public boolean isDisposed() {
-        return disposed.get();
+    public synchronized boolean isDisposed() {
+        return disposed;
     }
 
     @Override
@@ -49,32 +48,27 @@ public class Effect implements Runnable {
         ReactiveEnvInner env = ReactiveEnv.getInstance().get();
         if (threadId != null) {
             assert threadId == Thread.currentThread().getId() : "effect ran in wrong thread, try making it async";
-            cleanup.run();
-            env.batch(() -> env.effect(this, effect));
+            env.batch(() -> {
+                cleanup.run();
+                env.effect(this, effect);
+            });
         } else {
-            cleanup.run();
             env.batch(() -> {
                 synchronized (this) {
+                    cleanup.run();
                     env.effect(this, effect);
                 }
             });
         }
     }
 
-    Runnable getEffect() {
-        return effect;
-    }
-
     Long getThreadId() {
         return threadId;
     }
 
-    void addCleanup(Runnable runnable) {
-        if (!disposed.get())
+    synchronized void addCleanup(Runnable runnable) {
+        if (!disposed)
             cleanup.add(runnable);
-
-        if (disposed.get())
-            cleanup.run();
     }
 
     @Override
@@ -97,8 +91,8 @@ public class Effect implements Runnable {
     private static class Cleanup implements Runnable {
         private final Queue<Runnable> queue;
 
-        public Cleanup(Queue<Runnable> queue) {
-            this.queue = queue;
+        public Cleanup() {
+            this.queue = new ArrayDeque<>();
         }
 
         public void add(Runnable runnable) {
