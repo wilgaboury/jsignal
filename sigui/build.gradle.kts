b97d228118313a4f03d0e1c1357a1d7ecf581f06
Wil Gaboury <wgaboury@gmail.com>
2024-08-16 07:58:48 -0400

    Order dependencies and add linux arm support

--- sigui/build.gradle.kts
+++ sigui/build.gradle.kts
@@ -7,8 +7,8 @@ val skijaVersion = "0.116.1";
 val lwjglVersion = "3.3.3"
 
 val lwjglNatives = Pair(
-        System.getProperty("os.name")!!,
-        System.getProperty("os.arch")!!
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
 ).let { (name, arch) ->
     when {
         arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
@@ -16,13 +16,16 @@ val lwjglNatives = Pair(
                 "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
             else
                 "natives-linux"
-        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }                ->
+
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } ->
             "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-        arrayOf("Windows").any { name.startsWith(it) }                           ->
+
+        arrayOf("Windows").any { name.startsWith(it) } ->
             if (arch.contains("64"))
                 "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
             else
                 "natives-windows-x86"
+
         else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
     }
 }
@@ -45,9 +48,11 @@ dependencies {
 
     api("io.github.humbleui:skija-windows-x64:${skijaVersion}")
     api("io.github.humbleui:skija-linux-x64:${skijaVersion}")
+    api("io.github.humbleui:skija-linux-arm64:${skijaVersion}")
     api("io.github.humbleui:skija-macos-x64:${skijaVersion}")
-    api("io.github.humbleui:jwm:0.4.15")
     api("io.github.humbleui:skija-macos-arm64:${skijaVersion}")
+
+    api("io.github.humbleui:jwm:0.4.15")
 
     // 2024-03-26
     compileOnly(files("./hotswap-agent-1.4.2-SNAPSHOT.jar"))
