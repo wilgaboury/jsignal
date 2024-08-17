300b94e5e1dd252003abb68dd476518b1b413c1d
Wil Gaboury <wgaboury@gmail.com>
2024-08-16 21:53:05 -0400

    Simplify native library inclusion

--- sigui/build.gradle.kts
+++ sigui/build.gradle.kts
@@ -2,33 +2,16 @@ plugins {
     id("java-library")
 }
 
-val skijaVersion = "0.116.1";
+val skijaVersion = "0.116.1"
 
 val lwjglVersion = "3.3.3"
-
-val lwjglNatives = Pair(
-    System.getProperty("os.name")!!,
-    System.getProperty("os.arch")!!
-).let { (name, arch) ->
-    when {
-        arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
-            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
-                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
-            else
-                "natives-linux"
-
-        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } ->
-            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-
-        arrayOf("Windows").any { name.startsWith(it) } ->
-            if (arch.contains("64"))
-                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-            else
-                "natives-windows-x86"
-
-        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
-    }
-}
+val lwjglNatives = listOf(
+    "natives-windows-x86",
+    "natives-linux-x86",
+    "natives-linux-arm64",
+    "natives-macos-x86",
+    "natives-macos-arm64"
+)
 
 repositories {
     mavenCentral()
@@ -43,8 +26,13 @@ dependencies {
     implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
     implementation("org.lwjgl", "lwjgl")
     implementation("org.lwjgl", "lwjgl-yoga")
-    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-yoga", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-nfd")
+
+    for (classifier in lwjglNatives) {
+        runtimeOnly("org.lwjgl", "lwjgl", classifier = classifier)
+        runtimeOnly("org.lwjgl", "lwjgl-yoga", classifier = classifier)
+        runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = classifier)
+    }
 
     api("io.github.humbleui:skija-windows-x64:${skijaVersion}")
     api("io.github.humbleui:skija-linux-x64:${skijaVersion}")
